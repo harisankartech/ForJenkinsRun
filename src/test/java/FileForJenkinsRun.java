f646f263efc4c4eb3697f293c50cc1453f12a022
harisankarr <harisankartech@gmail.com>
2026-03-25 14:25:58 +0530

    first commit

---
+++ src/test/java/FileForJenkinsRun.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FileForJenkinsRun {
+
+    @Test
+    public void Demo(){
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement dropdown = driver.findElement(By.id("dropdown"));
+        Select select = new Select(dropdown);
+        select.selectByVisibleText("Option 1");
+
+        System.out.println(driver.getPageSource());
+    }
+}
